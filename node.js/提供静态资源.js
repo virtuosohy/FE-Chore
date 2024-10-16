f7a59c3d4a2f33e284c6f68ce6dcd7a971ba99d6
virtuosohy <virtuosohy@outlook.com>
2024-10-16 23:33:01 +0800

    10.16

--- node.js/提供静态资源.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('./初识'));
-
-app.listen(80, () => {
-  console.log('Server is running at http://127.0.0.1');
-});
